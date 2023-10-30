7a3d5abe53f0cbfc153a4f5eeeed9038f9b059c6
Sarbjot <sarbjotsinghofficial@gmail.com>
2023-10-31 03:32:19 +0530

    Enabled Swagger

--- src/main/java/com/dag/productservice/ProductserviceApplication.java
+++ src/main/java/com/dag/productservice/ProductserviceApplication.java
@@ -1,9 +1,11 @@
 package com.dag.productservice;
 
+import io.swagger.v3.oas.models.annotations.OpenAPI30;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPI30
 public class ProductserviceApplication {
 
 	public static void main(String[] args) {
