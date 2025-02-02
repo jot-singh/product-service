1b206f156c16369b085c10216141ac1a18b45f9d
Sarbjot <singh.sarbjot.94@gmail.com>
2025-02-02 15:30:31 +0530

    Added Oauth2 dependencies And Security Configurations

---
+++ src/main/java/com/dag/productservice/security/JwtObject.java
@@ -0,0 +1,18 @@
+package com.dag.productservice.security;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+public class JwtObject {
+    private String email;
+    private Long userId;
+    private Date createdAt;
+    private Date expiryAt;
+    private List<Role> roles = new ArrayList<>();
+}
---
+++ src/main/java/com/dag/productservice/security/Role.java
@@ -0,0 +1,11 @@
+package com.dag.productservice.security;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Role {
+    private Long id;
+    private String role;
+}
