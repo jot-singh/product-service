256b36d497c57e425c6438e1b938e2482490bca9
Sarbjot <singh.sarbjot.94@gmail.com>
2025-02-02 19:03:42 +0530

    Moved Classes to accurate package

--- src/main/java/com/dag/productservice/dao/schema/LocalProductRepository.java
+++ src/main/java/com/dag/productservice/dao/schema/LocalProductRepository.java
@@ -1,4 +1,4 @@
-package com.dag.productservice.dao;
+package com.dag.productservice.dao.schema;
 
 import com.dag.productservice.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
