d3e04a3f4c47b6ea5f06aeeadc90875db3fb0e80
Sarbjot <singh.sarbjot.94@gmail.com>
2025-08-29 01:00:07 +0530

    feat(security): Add OpenAPI dependency and configure Swagger access in security settings

--- src/main/java/com/dag/productservice/security/SecurityConfig.java
+++ src/main/java/com/dag/productservice/security/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
